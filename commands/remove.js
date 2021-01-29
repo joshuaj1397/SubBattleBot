05dc3a952396ba13d897e74b7b4a559563921f07
Josh <joshuaj1397@gmail.com>
2021-01-29 07:53:18 -0600

    Added remove user from battle command

--- commands/remove.js
+++ commands/remove.js
@@ -6,6 +6,33 @@ module.exports = {
             console.log('You don\'t have permission to use that');
             return 'You don\'t have permission to use that';
         }
-        // If user is in SubBattle delete them, otherwise return not found
+        const chessUsername = args[0];
+        try {
+            const collection = mongoClient.db('SubBattle').collection('Pools');
+            const subBattlePool = await collection.findOne({
+                guildId: message.guild.id,
+                channelId: message.channel.id,
+            });
+            if (!subBattlePool.members || !subBattlePool.members.some(member => member.username === chessUsername)) {
+                console.log('That user is not in the SubBattle pool');
+                res = 'That user is not in the SubBattle pool';
+            } else {
+                await collection.updateOne(subBattlePool,
+                    {
+                        $pull: {
+                            members: {
+                                username: chessUsername,
+                            },
+                        }
+                    });
+                console.log('Removed from the SubBattle pool');
+                res = 'Removed from the SubBattle pool';
+            }
+        } catch (err) {
+            console.log('Failed in db updates ' + err);
+            res = 'There was a database error, contact @Blind#6910';
+        } finally {
+            return res;
+        }
     }
 }
